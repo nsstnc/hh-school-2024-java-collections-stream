990adc5d81e2d6b709261a751eea3fe43ef9ed9b
nsstnc <egorgolubev0484@gmail.com>
2024-12-11 02:01:04 +0300

    Task 5 completed

--- src/main/java/tasks/Task5.java
+++ src/main/java/tasks/Task5.java
@@ -6,6 +6,7 @@ import common.PersonConverter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
 Задача 5
@@ -23,6 +24,13 @@ public class Task5 {
   }
 
   public List<ApiPersonDto> convert(List<Person> persons, Map<Integer, Integer> personAreaIds) {
-    return new ArrayList<>();
+    return persons.stream()
+        .map(person -> {
+          ApiPersonDto dto = personConverter.convert(person);
+          Integer areaId = personAreaIds.get(person.id());
+          dto.setAreaId(areaId);
+          return dto;
+        })
+        .collect(Collectors.toList());
   }
 }
